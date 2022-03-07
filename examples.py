2d20250d069a821264dcf420cc1b5bdb53d43de3
mrlegohead0x45 <mrlegohead0x45@gmail.com>
2022-03-07 20:04:12 +0000

    Create in and string packages
    Create subpackage for input streams. It raises NotImplementedError
    because I haven't implemented it yet. Also add some more examples and
    a docstring for the out subpackage

---
+++ examples.py
@@ -0,0 +1,35 @@
+import iostream
+
+# basic examples
+iostream.cout << "hello world" << iostream.endl
+iostream.cout << 5 << "\n"
+s = "hello world\n"
+iostream.cout << s
+
+# example of a streamable type
+class ExampleStreamableType:
+    _iostream_streamable_from = True
+    value = 8
+
+    def _iostream_stream_from(self, ostream: iostream.OutStream):
+        ostream << "hello from ExampleStreamableType" << iostream.endl
+        ostream << "value is " << self.value
+
+    # binary left shift still usable
+    def __lshift__(self, other: int):
+        return self.value << other
+
+var = ExampleStreamableType()
+iostream.cout << var << iostream.endl
+
+
+# files
+ofs = iostream.OutFileStream()
+ofs.open("test.txt")
+ofs << "hello world\n"
+ofs.close()
+
+# it has a context manager too
+with iostream.OutFileStream() as ofs:
+    ofs.open("test.txt", "a")
+    ofs << "hello from the outfilestream context manager" << iostream.endl
--- iostream/out/__init__.py
+++ iostream/out/__init__.py
@@ -1,3 +1,5 @@
+"""This is an interface package to the real code which is in _out to avoid import loops"""
+
 __all__ = ["OutStream", "OutFileStream", "cout"]
 
 from .._out.cout import _Cout
