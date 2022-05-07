4fcc731fe62d149b825348d1d5524bed1d9891db
mrlegohead0x45 <mrlegohead0x45@gmail.com>
2022-05-07 17:47:40 +0100

    Replace stream

--- tests/test_cout.py
+++ tests/test_cout.py
@@ -1,9 +1,17 @@
 import sys
 
-from cppstream import cout
+import pytest
+
+from cppstream import cout as _real_cout
 
 
-def test_cout(capfd):
+@pytest.fixture
+def cout():
+    _real_cout._stream = sys.stdout
+    return _real_cout
+
+
+def test_cout(cout, capsys):
     # with(capfd).disabled():
     # cout._stream = sys.stdout
     cout << "Hello World!"
@@ -12,5 +20,14 @@ def test_cout(capfd):
     # print("Hello World!")
     # with capfd.disabled():
     # print(cout.stream is sys.stdout) # False
-    cap = capfd.readouterr()
+    cap = capsys.readouterr()
+    assert cap.out == "Hello World!"
+
+
+def test_cout_stream_replace(capsys):
+    _real_cout._stream = sys.stdout
+
+    _real_cout << "Hello World!"
+
+    cap = capsys.readouterr()
     assert cap.out == "Hello World!"
