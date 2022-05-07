77e916ddaf49d72106cfaee9ce2c0505859d45c8
mrlegohead0x45 <mrlegohead0x45@gmail.com>
2022-05-07 17:26:21 +0100

    Use capfd

--- tests/test_cout.py
+++ tests/test_cout.py
@@ -1,11 +1,16 @@
+import sys
+
 from cppstream import cout
 
 
-def test_cout(capsys):
-    # with capsys.disabled():
+def test_cout(capfd):
+    # with(capfd).disabled():
+    # cout._stream = sys.stdout
     cout << "Hello World!"
 
     # cout.stream.flush()
     # print("Hello World!")
-    cap = capsys.readouterr()
+    # with capfd.disabled():
+    # print(cout.stream is sys.stdout) # False
+    cap = capfd.readouterr()
     assert cap.out == "Hello World!"
